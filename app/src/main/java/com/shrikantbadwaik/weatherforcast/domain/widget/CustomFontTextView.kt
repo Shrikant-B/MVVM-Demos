7081deb595d15b7746cdfdc928344d7a8030d793
Shrikant Badwaik <shrikant.badwaik@gmail.com>
2019-03-04 16:36:19 +0530

    custom fonts applied

---
+++ app/src/main/java/com/shrikantbadwaik/weatherforcast/domain/widget/CustomFontTextView.kt
@@ -0,0 +1,40 @@
+package com.shrikantbadwaik.weatherforcast.domain.widget
+
+import android.content.Context
+import android.graphics.Typeface
+import android.support.v7.widget.AppCompatTextView
+import android.util.AttributeSet
+import com.shrikantbadwaik.weatherforcast.R
+
+
+class CustomFontTextView : AppCompatTextView {
+    constructor(context: Context) : super(context) {
+        init(null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet?) {
+        attrs?.let {
+            val a = context.obtainStyledAttributes(attrs, R.styleable.CustomFontTextView)
+            val fontName = a.getString(R.styleable.CustomFontTextView_customFontName)
+
+            try {
+                fontName?.let {
+                    val myTypeface = Typeface.createFromAsset(context.assets, "fonts/$it")
+                    typeface = myTypeface
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            a.recycle()
+        }
+    }
+}
